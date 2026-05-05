34da19f1cc9e35fe1a62bb5b8f37a68d9f87a5fe
anishdhandore <anishdhandore@users.noreply.github.com>
2026-05-05 05:04:13 +0000

    deploy: 66ea2395ef977f04cf7af8500b66f543938c697d

---
+++ precache-manifest.8b252b37c99fe666acd31b0ec4fa9566.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "179763ee21205bd904b5",
+    "url": "/static/js/main.179763ee.chunk.js"
+  },
+  {
+    "revision": "7143af38f918f5482f93",
+    "url": "/static/css/1.b71fc1c0.chunk.css"
+  },
+  {
+    "revision": "7143af38f918f5482f93",
+    "url": "/static/js/1.7143af38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80168b4b8f7b0f3230731157f5b82411",
+    "url": "/static/media/profile_icon_no_bg.80168b4b.png"
+  },
+  {
+    "revision": "7741456e7ea19180e90a94069f6a05b4",
+    "url": "/static/media/me.7741456e.jpg"
+  },
+  {
+    "revision": "598cebe1fb1c19fc27c5ca5fb4cd80a2",
+    "url": "/static/media/swift.598cebe1.jpg"
+  },
+  {
+    "revision": "f7ab478d3397ddd9a53579768b0748ed",
+    "url": "/static/media/csusm.f7ab478d.jpg"
+  },
+  {
+    "revision": "699d279ab17accc0eac9cdacd475c774",
+    "url": "/static/media/sgs.699d279a.jpg"
+  },
+  {
+    "revision": "3d34556d0ce532f7f3c3d9ed4b4befb6",
+    "url": "/static/media/soccer.3d34556d.jpg"
+  },
+  {
+    "revision": "da57f9df92bf1872b11962fa7c82f05f",
+    "url": "/static/media/dogs.da57f9df.jpg"
+  },
+  {
+    "revision": "2714c4e3a32d74245b8a884132750b0d",
+    "url": "/static/media/safespace.2714c4e3.jpg"
+  },
+  {
+    "revision": "c57f136544ac5fa43629e4bff13ba26e",
+    "url": "/static/media/gcodes.c57f1365.jpg"
+  },
+  {
+    "revision": "f8cd3252d94f3d5153a7019f5de08558",
+    "url": "/static/media/zombie.f8cd3252.jpg"
+  },
+  {
+    "revision": "037e36b76563a8bac14b21d0def78202",
+    "url": "/static/media/artistic.037e36b7.jpg"
+  },
+  {
+    "revision": "f1d1df2e141d869ff6657969510f5452",
+    "url": "/static/media/DanceAI.f1d1df2e.png"
+  },
+  {
+    "revision": "67c2e5e2c5009f6da0ef6b64731731e4",
+    "url": "/static/media/open-sans-cyrillic-ext-400-normal.67c2e5e2.woff2"
+  },
+  {
+    "revision": "a68519222b370fef3e6da827dfdb311b",
+    "url": "/static/media/open-sans-cyrillic-ext-400-normal.a6851922.woff"
+  },
+  {
+    "revision": "7e05f5267bba1aa9fb260096f923bdb7",
+    "url": "/static/media/open-sans-cyrillic-400-normal.7e05f526.woff2"
+  },
+  {
+    "revision": "7e04c84799bb1bd87444ee7436aae9d3",
+    "url": "/static/media/open-sans-cyrillic-400-normal.7e04c847.woff"
+  },
+  {
+    "revision": "41564e9a132ed7abf8bb3d3071f8ef18",
+    "url": "/static/media/open-sans-greek-ext-400-normal.41564e9a.woff2"
+  },
+  {
+    "revision": "f4c48b1d32e658eae7d5c27feaee58b6",
+    "url": "/static/media/open-sans-greek-ext-400-normal.f4c48b1d.woff"
+  },
+  {
+    "revision": "3d0cb57cde2b7d871cc5cd3eb38a6228",
+    "url": "/static/media/open-sans-greek-400-normal.3d0cb57c.woff"
+  },
+  {
+    "revision": "80210d1ff4de56c6704607f3831e8f6c",
+    "url": "/static/media/open-sans-hebrew-400-normal.80210d1f.woff2"
+  },
+  {
+    "revision": "acf2ace2126d9409bc8b48dcb3e5fc16",
+    "url": "/static/media/open-sans-hebrew-400-normal.acf2ace2.woff"
+  },
+  {
+    "revision": "68a005605ca4a121fb3dc41ec25c5138",
+    "url": "/static/media/open-sans-math-400-normal.68a00560.woff"
+  },
+  {
+    "revision": "efb3f28447c9ef35fd5882fb763b37c3",
+    "url": "/static/media/open-sans-math-400-normal.efb3f284.woff2"
+  },
+  {
+    "revision": "31c3253c6146d2a15cedfcfbc975ef7d",
+    "url": "/static/media/open-sans-symbols-400-normal.31c3253c.woff2"
+  },
+  {
+    "revision": "01209eacad09a3dae36099edcb8e3790",
+    "url": "/static/media/open-sans-vietnamese-400-normal.01209eac.woff"
+  },
+  {
+    "revision": "776fdc253d54124dd63f274bf5ea35f0",
+    "url": "/static/media/open-sans-vietnamese-400-normal.776fdc25.woff2"
+  },
+  {
+    "revision": "8655d20bbcc8cdbfab17b6be6cf55df3",
+    "url": "/static/media/open-sans-latin-400-normal.8655d20b.woff2"
+  },
+  {
+    "revision": "e922d0164692bbf15dfa42c3c4427697",
+    "url": "/static/media/open-sans-latin-400-normal.e922d016.woff"
+  },
+  {
+    "revision": "7d0448945bef4ea0a8e2beda8445a337",
+    "url": "/static/media/open-sans-symbols-400-normal.7d044894.woff"
+  },
+  {
+    "revision": "c59233b2655bf8e3f3b7756081961fbc",
+    "url": "/static/media/open-sans-latin-ext-400-normal.c59233b2.woff"
+  },
+  {
+    "revision": "be7b70ab1265b1047bd93422397c655e",
+    "url": "/static/media/open-sans-latin-ext-400-normal.be7b70ab.woff2"
+  },
+  {
+    "revision": "cbca61f27f93fd71171fe8d314c66ab9",
+    "url": "/static/media/open-sans-greek-400-normal.cbca61f2.woff2"
+  },
+  {
+    "revision": "530cc7e6dd37320f212482e6928de3f1",
+    "url": "/index.html"
+  }
+];
